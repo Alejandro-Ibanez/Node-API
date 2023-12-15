abfa4566bd35326d2cb9ad2a5c4000143eeaf436
Alejandro Ibañez <ai.alejandro.ibanez@gmail.com>
2023-12-15 00:16:45 -0400

    Initial commit

---
+++ routes/tracks.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const customHeader = require("../middleware/customHeader");
+const {
+  validatorCreateItem,
+  validatorGetItem,
+} = require("../validators/tracks");
+const {
+  getItems,
+  getItem,
+  createItem,
+  updateItem,
+  deleteItem,
+} = require("../controllers/tracks");
+
+/**
+ * Lista los items
+ */
+router.get("/", getItems);
+/**
+ * Obtener detalle de item
+ */
+router.get("/:id", validatorGetItem, getItem);
+/**
+ * Crea un registro
+ */
+router.post("/", validatorCreateItem, createItem);
+/**
+ * Actualizar un registro
+ */
+router.put("/:id", validatorGetItem, validatorCreateItem, updateItem);
+/**
+ * Eliminar un item
+ */
+router.delete("/:id", validatorGetItem, deleteItem);
+
+module.exports = router;
